0c8bb28f593edb0ec68d9b408b92187d30351782
Adam Breen <adam@adambreen.com>
2012-11-24 18:06:41 +1000

    affiliate js now in place for info page

--- public/javascripts/affiliate.js
+++ public/javascripts/affiliate.js
@@ -2,7 +2,14 @@
 $(document).ready(function (){
 
 	var
-		affiliateInput = $('#00N90000004H2pU')
+		affiliateInput = $('#00N90000004H2pU'),
+		matches        = document.URL.match(/https?:\/\/.*choicepoint.jp\/info\/?(.*)/),
+		affiliateCode
 	;
+
+	if(matches.length){
+		affiliateCode = matches[1];
+		affiliateInput.val(affiliateCode);
+	}
 
 });
