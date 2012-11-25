f3388b5de0053863bc79ff4672d7c5c7e7aaed82
Adam Breen <adam@adambreen.com>
2012-11-25 15:19:12 +1000

    updated affiliate.js to handle ?utm= for our internal lists

--- public/javascripts/affiliate.js
+++ public/javascripts/affiliate.js
@@ -3,12 +3,17 @@ $(document).ready(function (){
 
 	var
 		affiliateInput = $('#00N90000004H2pU'),
-		matches        = document.URL.match(/https?:\/\/.*choicepoint.jp\/(info|test)\/?(.*)/),
+		matches        = document.URL.match(/https?:\/\/.*\/(info|test)\/?(.*)/),
 		affiliateCode
 	;
 
 	if(matches && matches.length > 1){
 		affiliateCode = matches[2];
+
+		if(affiliateCode[0] === '?'){
+			affiliateCode = 'internalList:' + affiliateCode.substr(1).split('&')[0].split('=')[1];
+		}
+
 		affiliateInput.val(function (index, value) { return affiliateCode; });
 	}
 
